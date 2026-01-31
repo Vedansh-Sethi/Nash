e80bdd091033bbecc10bd1992894d3ed80f00435
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-01-31 06:31:58 +0530

    Added message cards and textfield for discussion page

--- app/lib/pages/bet/bet_discussion.dart
+++ app/lib/pages/bet/bet_discussion.dart
@@ -1,12 +1,99 @@
+import 'package:app/config/theme.dart';
+import 'package:app/widgets/normal_text_field.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/message_card.dart';
 
 class BetDiscussionPage extends StatelessWidget {
   const BetDiscussionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("DISCUSSION PAGE", style: TextStyle(color: Colors.white)),
+    List<Map<String, dynamic>> messages = [
+      {
+        "sender_id": "id1",
+        "content": "My name is User 1",
+        "created_at": DateTime.now(),
+      },
+      {
+        "sender_id": "id1",
+        "content": "Hello There",
+        "created_at": DateTime.now().subtract(Duration(seconds: 20)),
+      },
+      {
+        "sender_id": "id2",
+        "content": "Hello There mate",
+        "created_at": DateTime.now().subtract(Duration(minutes: 2)),
+      },
+      {
+        "sender_id": "id1",
+        "content": "So excited to be here",
+        "created_at": DateTime.now().subtract(Duration(minutes: 5)),
+      },
+      {
+        "sender_id": "id2",
+        "content": "Lets welcome him",
+        "created_at": DateTime.now().subtract(Duration(minutes: 7)),
+      },
+      {
+        "sender_id": "id2",
+        "content": "We have another member amongst us",
+        "created_at": DateTime.now().subtract(Duration(minutes: 8)),
+      },
+      {
+        "sender_id": "id2",
+        "content": "We have another member amongst us",
+        "created_at": DateTime.now().subtract(Duration(minutes: 8)),
+      },
+      {
+        "sender_id": "id2",
+        "content":
+            "We have another member amongst us. And some pretty long text which I don't know why even it is there but it still is smaller than Lorem Ipsum",
+        "created_at": DateTime.now().subtract(Duration(minutes: 8)),
+      },
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              shrinkWrap: true,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return MessageCard(
+                  message: messages[index],
+                  displayUsername:
+                      index == messages.length - 1 ||
+                      messages[index]["sender_id"] !=
+                          messages[index + 1]["sender_id"],
+                  displayTime:
+                      index == 0 ||
+                      messages[index - 1]["sender_id"] !=
+                          messages[index]["sender_id"],
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(child: CustomTextField(hintText: "Send a message...")),
+              const SizedBox(width: 8),
+              IconButton(
+                padding: EdgeInsets.all(10),
+                icon: Icon(Icons.send),
+                onPressed: () {},
+                style: FilledButton.styleFrom(
+                  backgroundColor: context.colorScheme.surface,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ app/lib/pages/bet/widgets/message_card.dart
@@ -0,0 +1,65 @@
+import 'package:app/config/theme.dart';
+import 'package:app/extensions/datetime.dart';
+import 'package:flutter/material.dart';
+
+const String currentUser = "id1";
+
+class MessageCard extends StatelessWidget {
+  final Map<String, dynamic> message;
+  final bool displayTime;
+  final bool displayUsername;
+
+  const MessageCard({
+    super.key,
+    required this.message,
+    this.displayTime = false,
+    this.displayUsername = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bool ownMessage = currentUser == message["sender_id"];
+
+    return Align(
+      alignment: ownMessage ? Alignment.centerRight : Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: ownMessage
+            ? CrossAxisAlignment.end
+            : CrossAxisAlignment.start,
+        children: [
+          if (displayUsername)
+            Text(
+              message["sender_id"],
+              style: TextStyle(color: context.colorScheme.onSurfaceVariant),
+            ),
+          Card(
+            color: ownMessage
+                ? context.colorScheme.primary
+                : context.colorScheme.secondary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(ownMessage ? 0 : 20),
+                topLeft: Radius.circular(ownMessage ? 20 : 0),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              padding: EdgeInsets.all(16),
+              child: Text(
+                message["content"],
+                style: TextStyle(color: context.colorScheme.onPrimary),
+              ),
+            ),
+          ),
+          if (displayTime)
+            Text(
+              (message["created_at"] as DateTime).toReadableFormat(),
+              style: TextStyle(color: context.colorScheme.onSurfaceVariant),
+            ),
+        ],
+      ),
+    );
+  }
+}
