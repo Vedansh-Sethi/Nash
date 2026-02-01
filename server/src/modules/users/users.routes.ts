4fd46c1715cde9c96de6a74f67a8d9efc8b6ffa2
Keshav Bansal <130533959+kingspin-42@users.noreply.github.com>
2026-02-01 12:32:03 +0530

    re

--- server/src/modules/users/users.routes.ts
+++ server/src/modules/users/users.routes.ts
@@ -4,14 +4,6 @@ import * as userControllers from "./users.controller.js";
 
 const router = Router();
 
-<<<<<<< HEAD
-router.post('/check_in', authenticate, userControllers.dailyCheckIn)
-router.get('/groups', authenticate, userControllers.getGroups)
-router.get('/placed_bets', authenticate, userControllers.getUserPlacedOpenBets)
-router.get('/created_bets', authenticate, userControllers.getUserCreatedOpenBets)
-router.get("/me", authenticate, userControllers.getCurrentUser);
-router.get('/:user_id', authenticate, userControllers.getUser)
-=======
 router.post("/check_in", authenticate, userControllers.dailyCheckIn);
 router.get("/groups", authenticate, userControllers.getGroups);
 router.get("/placed_bets", authenticate, userControllers.getUserPlacedOpenBets);
@@ -22,6 +14,5 @@ router.get(
 );
 router.get("/me", authenticate, userControllers.getCurrentUser);
 router.get("/:user_id", authenticate, userControllers.getUser);
->>>>>>> fc1232deeb972d68e778636a3518969be82fe9e1
 
 export default router;
