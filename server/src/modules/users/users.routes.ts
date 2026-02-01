6d4c83f1e43e167da126b683776e9c7c9f1dc6b4
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-02-01 11:25:59 +0530

    [fix] fixed /users/me

--- server/src/modules/users/users.routes.ts
+++ server/src/modules/users/users.routes.ts
@@ -1,14 +1,18 @@
-import {Router} from "express";
-import * as userControllers from "./users.controller.js";
+import { Router } from "express";
 import { authenticate } from "../../middleware/auth.middleware.js";
+import * as userControllers from "./users.controller.js";
 
 const router = Router();
 
-router.post('/check_in', authenticate, userControllers.dailyCheckIn)
-router.get('/groups', authenticate, userControllers.getGroups)
-router.get('/placed_bets', authenticate, userControllers.getUserPlacedOpenBets)
-router.get('/created_bets', authenticate, userControllers.getUserCreatedOpenBets)
-router.get('/:user_id', authenticate, userControllers.getUser)
+router.post("/check_in", authenticate, userControllers.dailyCheckIn);
+router.get("/groups", authenticate, userControllers.getGroups);
+router.get("/placed_bets", authenticate, userControllers.getUserPlacedOpenBets);
+router.get(
+  "/created_bets",
+  authenticate,
+  userControllers.getUserCreatedOpenBets,
+);
 router.get("/me", authenticate, userControllers.getCurrentUser);
+router.get("/:user_id", authenticate, userControllers.getUser);
 
 export default router;
